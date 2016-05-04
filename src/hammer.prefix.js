eff051e8a37374d4eee548d50014cfe230bef0e9
Howard Burrows <howard.burrows@gmail.com>
2016-05-04 16:34:06 -0700

    Rename 'factory' to 'expose' since the function isn't returning anything

--- src/hammer.prefix.js
+++ src/hammer.prefix.js
@@ -1,4 +1,4 @@
-( function( global, factory ) {
+( function( global, expose ) {
 
 	'use strict';
 
@@ -7,23 +7,24 @@
 	if ( typeof module === 'object' && typeof module.exports === 'object' ) {
 
 		// For CommonJS and CommonJS-like environments where a proper `window`
-		// is present, execute the factory and to setup Hammer.
+		// is present, execute the expose function to initialize Hammer and
+    // expose to environment via AMD, CommonJS, etc.
 		// For environments that do not have a `window` with a `document`
-		// (such as Node.js), expose a factory as module.exports.
+		// (such as Node.js), export a factory as module.exports.
 		// This accentuates the need for the creation of a real `window`.
 		// e.g. var hammertime = require("hammer")(window);
     if (global.document) {
-      factory( global, global.document, 'Hammer' );
+      expose( global, global.document, 'Hammer' );
     } else {
       module.exports = function( w ) {
 				if ( !w.document ) {
 					throw new Error( 'Hammer requires a window with a document' );
 				}
-				return factory( w, w.document, 'Hammer' );
+				return expose( w, w.document, 'Hammer' );
 			};
     }
 	} else {
-		factory( global, global.document, 'Hammer' );
+		expose( global, global.document, 'Hammer' );
 	}
 
 // Pass this if window is not defined yet
